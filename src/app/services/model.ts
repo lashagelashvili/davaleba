e33863de9b3425e0a323a819924a0845fe0f85aa
lashagelashvili <las.gelashvili@gmail.com>
2022-03-21 18:23:12 +0400

    add models

---
+++ src/app/services/model.ts
@@ -0,0 +1,29 @@
+export interface UserDetails {
+    id: number,
+    name: string,
+    username: string,
+    email: string,
+    address: UserAddress,
+    phone: number,
+    website: string,
+    company: Company
+}
+
+export interface UserAddress {
+    street: string,
+    suite: string,
+    city: string,
+    zipcode: string,
+    geo: Geo
+}
+
+export interface Geo {
+    lat: number,
+    lng: number
+}
+
+export interface Company {
+    name: string,
+    catchPhrase: string,
+    bs: string
+}
