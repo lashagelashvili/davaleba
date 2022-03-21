c9697d184d4f192ce65dccc4aaafcee60f645ad3
lashagelashvili <las.gelashvili@gmail.com>
2022-03-21 23:40:52 +0400

    add material module

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
+
+@NgModule({
+    imports: [],
+    exports: [
+        MatButtonModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatIconModule
+    ],
+    declarations: [],
+    providers: [],
+})
+export class MaterialModule { }
