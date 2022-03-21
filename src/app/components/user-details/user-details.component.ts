ac00c801a8265de3a0de54da92f72e1c3a177a50
lashagelashvili <las.gelashvili@gmail.com>
2022-03-21 23:48:18 +0400

    cleanup

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -6,25 +6,32 @@ import { UserDetails } from 'src/app/services/model';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.css']
+  styleUrls: ['./user-details.component.scss']
 })
 export class UserDetailsComponent implements OnInit {
 
-  currentId: number;
-  userDetails: UserDetails;
+  private userId: number;
+  public userDetails: UserDetails;
 
   constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
-    this.currentId = this.route.snapshot.params.id;
+    //get current user id
+    this.userId = this.route.snapshot.params.id;
 
-    this.apiService.getUserDetails(this.currentId).subscribe(data => {
+    //get user details data
+    this.apiService.getUserDetails(this.userId).subscribe(data => {
       this.userDetails = data;
-      console.log(this.userDetails)
     });
   }
 
-  onButtonClick() {
-    this.router.navigateByUrl(`user/${this.currentId}/posts`)
+  public onButtonClick() {
+    //navigate to user posts page
+    this.router.navigateByUrl(`user/${this.userId}/posts`)
+  }
+
+  //navigate back
+  public onBackClick() {
+    this.router.navigateByUrl('user')
   }
 }
